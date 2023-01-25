46d1d1d2c2847b8a6a9ba96d472c9bc7e6e67d54
crizylmae <102636290+crizylmae@users.noreply.github.com>
2023-01-25 12:50:48 +0800

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-from ssis.models.admin import Admin
-
-def admin_found(username: str, password: str) -> bool:
-    if Admin(username,password).registered_user():
-        return True
-    return False
-
